6f501770bbdb2c02d143eb87d62ebfe61d0a6549
amiar <amiarslimane@yandex.ru>
2020-03-21 18:29:17 +0100

    add gif example

--- app/src/main/java/com/example/bannerslider/MainActivity.java
+++ app/src/main/java/com/example/bannerslider/MainActivity.java
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
         mainActivity = this;
         imageViewBanner = findViewById(R.id.imageViewBanner);
 
-        imagesURLs.add("https://pixabay.com/get/5fe1d3454d51b10ff3d89938b977692b083edbed5756764c7c287e/banner-916673_640.jpg");
-        imagesURLs.add("https://pixabay.com/get/54e0dd414951ad14f1dc8460825668204022dfe05b55764e702673d0/banner-2082331_640.png");
-        imagesURLs.add("https://pixabay.com/get/54e8d4404e5aab14f1dc8460825668204022dfe05b55764e732f79d0/nature-2813487_640.jpg");
-        imagesURLs.add("https://pixabay.com/get/57e6dd454356af14f1dc8460825668204022dfe05b55764e70267dd5/banner-1686943_640.jpg");
-        imagesURLs.add("https://d33wubrfki0l68.cloudfront.net/4d881a9d53e300fb2d86990647a3853bb4ec8adb/3e7f0/static/blog/posts/how-to-create-high-quality-gifs/noblur.gif");
+        imagesURLs.add("https://raw.githubusercontent.com/amiarSlimane/images-slider/master/images/0.jpg");
+        imagesURLs.add("https://raw.githubusercontent.com/amiarSlimane/images-slider/master/images/1.jpg");
+        imagesURLs.add("https://raw.githubusercontent.com/amiarSlimane/images-slider/master/images/3.png");
+        imagesURLs.add("https://raw.githubusercontent.com/amiarSlimane/images-slider/master/images/4.jpg");
+        imagesURLs.add("https://raw.githubusercontent.com/amiarSlimane/images-slider/master/images/noblur.gif");
     }
 
     @Override
